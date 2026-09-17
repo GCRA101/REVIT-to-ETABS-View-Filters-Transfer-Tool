7cbb7f69047e5214168f120a4a7645ee3566b346
g.albieri <126593217+GCRA101@users.noreply.github.com>
2026-09-17 16:01:27 +0100

    Remove unused references in ViewFiltersTransfer\Command.cs

--- ViewFiltersTransfer/Command.cs
+++ ViewFiltersTransfer/Command.cs
@@ -2,30 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-// Libraries for UI Buttons/Panels
-using System.Reflection;
-using System.Xaml;
 // Libraries for Revit
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using Autodesk.Revit.DB.Events;
-using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 // Libraries for ETABS
 using ETABSv1;
 // Other Libraries
-using System.Linq.Expressions;
-using static System.Net.Mime.MediaTypeNames;
-using System.Windows.Forms;
-using System.Windows.Media;
-using System.Xml.Linq;
-using System.Runtime.CompilerServices;
-using Autodesk.Revit.Creation;
-using System.Security.Cryptography;
 using View = Autodesk.Revit.DB.View;
-using System.Collections;
 
 namespace ViewFiltersTransfer
 {
